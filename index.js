704bbb5584216fbc3fb4627be8fd463f9b94bfab
amit kumar <107244837+amitsingh2775@users.noreply.github.com>
2025-03-20 18:48:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ require('dotenv').config();
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server, { cors: { origin: "https://connect-match-frontend.vercel.app/" },transports: ["websocket"],
+const io = new Server(server, { cors: { origin: "https://connect-match-frontend.vercel.app/" },transports: ["polling"],
   pingInterval: 60000,
   pingTimeout: 20000,});
 const port=8000
